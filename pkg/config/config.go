fcd3d469e199407bff40b77a8ed572115a1f324d
Teran Sarathchandra <teran8777@gmail.com>
2026-03-10 05:48:14 +0530

    Minor Changes

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -108,7 +108,7 @@ func Load() (*Config, error) {
 		GraphAPI: GraphAPIConfig{
 			BaseURL:   getGraphBaseURL(),
 			TimeoutMs: getEnvInt("GRAPH_API_TIMEOUT_MS", 15000),
-			Namespace: getEnv("OVERVIEW_NAMESPACE", "onlineboutique"),
+			Namespace: getEnv("OVERVIEW_NAMESPACE", "default"),
 		},
 		RateLimit: RateLimitConfig{
 			WindowMs:    getEnvInt("RATE_LIMIT_WINDOW_MS", 60000),
